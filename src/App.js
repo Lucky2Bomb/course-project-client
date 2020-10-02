d1d1e0cb4e8f685133d2efd4cfc807ce4475c5f0
Lucky2Bomb <Lucky2Bomb@gmail.com>
2020-10-02 12:26:43 +0500

    initial client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+const axios = require('axios');
+
+
 
 function App() {
+  const [message, setMessage] = useState("none");
+  const [serverMessage, setServerMessage] = useState("server message...");
+  const [item, setItem] = useState(null);
+
+  const messageEventHandler = () => {
+    setMessage(prev => prev + " none");
+  }
+
+  const serverMessageEventHandler = async () => {
+    try {
+      // Worked!
+      // const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita").then(res => res.json());
+      // console.log(response.drinks[1].strDrink);
+
+      // setServerMessage(response.drinks[1].strDrink);
+
+      const response = await fetch("http://localhost:3001").then(res => res.json());
+      setServerMessage(response.message);
+    } catch (error) {
+      setServerMessage("error: " + error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="сontainer">
+      <h1>{message}</h1>
+      <h1>{serverMessage}</h1>
+      <button className="btn btn-secondary" onClick={messageEventHandler}>Add msg</button>
+      <button className="btn btn-secondary" onClick={serverMessageEventHandler}>Call server</button>
     </div>
   );
 }
